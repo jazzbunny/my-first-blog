3bda0c5ca17cc8ee0ddbee8b488a91906a7733da
jazzbunny <monika.bialokowska@gmail.com>
2015-05-16 18:39:41 +0200

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 class Post(models.Model):
     author=models.ForeignKey("auth.User")
     title=models.CharField(max_length=200)
-    test=models.TextField(max_length=250)
+    text=models.TextField(max_length=250)
     created_date=models.DateTimeField(default=timezone.now)
     published_date=models.DateTimeField(blank=True,null=True)
     def publish(self):
         self.published_date=timezone.now()
         self.save()
     def __str__(self):
-        return self.title+" mom "
+        return "Post: " + self.title
